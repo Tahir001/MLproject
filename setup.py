0ed74ff9a45918d8d74ea6b42df6362678b381ad
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2023-06-30 20:02:10 -0400

    Add src folder and generic setup

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import find_packages, setup 
 from typing import List
 
+hypen = '-e .'
+
 def get_requirements(file_path) -> List[str]:
     """
     The following function will return the list of requirements
@@ -17,11 +19,14 @@ def get_requirements(file_path) -> List[str]:
         requirements= file_obj.readlines()
         requirements= [req.replace('\n', ' ') for req in requirements]
 
+        if hypen in requirements:
+            requirements.remove(hypen)
+
 setup(
-    name='mlproject'
-    version='0.0.1'
-    author='Tahir Muhammad'
-    author_email = 'Tahir.muhammad@mail.utoronto.ca'
-    packages = find_packages()
+    name='mlproject',
+    version='0.0.1',
+    author='Tahir Muhammad',
+    author_email = 'Tahir.muhammad@mail.utoronto.ca',
+    packages = find_packages(),
     install_requires = get_requirements('requirements.txt')
 )
