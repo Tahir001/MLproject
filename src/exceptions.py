b810ca90860ffffa24c429597020f7d80be1296a
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2023-07-02 02:23:02 -0400

    Update exceptions and logging files

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,7 +1,8 @@
 # Any exception that is getting controlled, sys library will have that information 
 import sys
+import logging
 
-def error_message_detail(error, error_detai:sys):
+def error_message_detail(error, error_detail:sys):
     # All information about the exception is stored in the variable
     _, _, exc_tb = error_detail.exc_info()
 
@@ -12,11 +13,9 @@ def error_message_detail(error, error_detai:sys):
 class CustomException(Exception):
 
     def __init__(self, error_message, error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message=error_message_detail(error_message, error_detail=error_detail)
 
     def __str__(self):
         return self.error_message()
-
-
 
--- src/logger.py
+++ src/logger.py
@@ -5,7 +5,7 @@ from datetime import datetime
 # Generate a log file for logs w/ corresponding current date 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 # We need to give a path on where the files are stored 
-logs_path = os.path.join(os.getcwd, "logs", LOG_FILE)
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 # Make this a directory and append to it if it already exsists 
 os.makedirs(logs_path, exist_ok=True)
 
