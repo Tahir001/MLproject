a783be16e662f1b737aaf6d39f96320f5b5fb345
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2023-07-05 19:54:29 -0400

    Write Setters and Getters for Data Ingestion Class

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,5 +12,25 @@ from dataclasses import dataclass
 from src.exceptions import CustomException
 from src.logger import logging
 
+@dataclass
 class DataIngestionConfig:
-    pass
+    # Inputs that we're giving to our data ingestion component -> Now it knows where to save these files
+    train_data_path: str=os.path.join("artifacts", "train.csv")
+    test_data_path: str=os.path.join("artifacts", "test.csv")
+    raw_data_path: str=os.path.join("artifacts", "data.csv")
+
+class DataIngestion:
+    def __init__(self):
+        # The three paths will be stored in our variable here 
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        # Start our data ingestion process 
+        logging.info("Entered the Data Ingestion Method or Component")
+        try:
+            df = pd.read_csv('')
+        except:
+            pass
+
+
+
