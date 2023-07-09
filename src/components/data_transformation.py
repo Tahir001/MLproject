dff81c8c868a3b0d479c5602baf36f0a33c37f24
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2023-07-09 13:42:02 -0400

    Add data transformation class

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,5 +1,6 @@
 # Import standard libraries 
 import sys
+import os
 import pandas as pd
 import numpy as np
 
@@ -11,12 +12,58 @@ from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
-# MLOps
+# MLops
 from src.exceptions import CustomException
 from src.logger import logging
 
+@dataclass
 class DataTransformationConfig:
-    os.path.job
+    """
+    Gives us any path or input that we will be requiring for our data & transformation component 
+    """
+    # To save any models in a pkl file, for this we need a specific pkl path 
+    # You can also do the same for model file 
+    preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
+
+class DataTransformation:
+    # Give the input from above to here
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        # Create all of my pkl files which are responsible for our data transformation
+        try:
+            # Must do EDA before this
+            numerocical_columsn = ['writing_score', 'reading_score']
+            categorical_columns = [
+                'gender', 
+                'race_ethnicity',
+                'parental_level_of_education',
+                'lunch',
+                'test_preperation_course'
+            ]
+            num_pipeline = Pipeline(
+                steps=[
+                    ('imputer', SimpleImputer(strategy='median'))
+                    ('scaler', StandardScaler())
+                ]
+            )
+
+            cat_pipeline = Pipeline(
+                steps = [
+                    ('imputer', SimpleImputer(strategy='most_frequent')),
+                    ('one_hot_encoder', OneHotEncoder()),
+                    ('scaler', StandardScaler())
+                ]
+            )
+            
+        except:
+            pass
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = 
+
 
     def __init__(self): 
 
