182a3f87fed57610f2e7a29e7865c9d3d51f1b6c
Tahir Muhammad <Tahir.muhammad@mail.utoronto.ca>
2023-07-17 23:52:44 -0400

    Add Model Trainer Configurations

--- src/components/model.trainer.py
+++ src/components/model.trainer.py
@@ -1,7 +1,9 @@
+# Standard libraries
 import os
 import sys
 from dataclasses import dataclass
 
+# Models and Metrics
 from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
@@ -14,6 +16,27 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 
+# MLOps 
 from src.exceptions import CustomException
 from src.logger import logging
+from src.utils import save_object
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
+
+class ModelTrainer:
+
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train, test_array, preprocessor_path):
+        try:
+            # Log some info
+            logging.info("Splitting training and test input data")
+            pass
+        except Exception as e: 
+            raise CustomException(e, sys)
+
 
